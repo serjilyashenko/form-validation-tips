c536430ad4ee74c601890a8e47b7890c4bc87da6
serjilyashenko <serjilyashenko@users.noreply.github.com>
2023-09-09 23:36:18 +0200

    feat: script and styles

---
+++ script.js
@@ -0,0 +1,12 @@
+(function () {
+  console.log('Hello!');
+
+  const constraintForm = document.getElementById('constraint-form');
+
+  constraintForm.setAttribute('novalidate', '');
+
+  constraintForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log('>> submit');
+  });
+})();
